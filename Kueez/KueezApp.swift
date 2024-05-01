7b19e4a08aea8ddf5b191ecc96651f61f0a7b2ae
yusuf <yusuferkoc99@gmail.com>
2024-05-01 16:09:09 +0300

    Initial Commit

---
+++ Kueez/KueezApp.swift
@@ -0,0 +1,17 @@
+//
+//  KueezApp.swift
+//  Kueez
+//
+//  Created by Yusuf Erkoç on 1.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct KueezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
